16e14fcfcfd91c1c4f74a1e05a0b839e05147e69
Derek <nightwing917@hotmail.com>
2024-10-04 19:52:16 -0600

    adding template literals

--- script.js
+++ script.js
@@ -14,8 +14,34 @@ function displayUser(user) {
     document.body.style.backgroundColor = 'royalblue';
   }
 
-  userDisplay.innerHTML = '';
+  userDisplay.innerHTML = '
+    <div class="flex justify-between">
+          <div class="flex">
+            <img
+              class="w-48 h-48 rounded-full mr-8"
+              src="https://randomuser.me/api/portraits/women/45.jpg"
+            />
+            <div class="space-y-3">
+              <p class="text-xl">
+                <span class="font-bold">Name: </span>${user.name.first} ${user.name.last}
+              </p>
+              <p class="text-xl">
+                <span class="font-bold">Email: </span>${user.email}
+              </p>
+              <p class="text-xl">
+                <span class="font-bold">Phone: </span> (555) 555-5555
+              </p>
+              <p class="text-xl">
+                <span class="font-bold">Location: </span> Boston MA
+              </p>
+              <p class="text-xl"><span class="font-bold">Age: </span> 30</p>
+            </div>
+          </div>
+        </div>
+  ';
 }
 
 
 fetchUser();
+
+       
