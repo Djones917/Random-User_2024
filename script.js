24304e2f75f015f68557b4c74d65904dadbbd3a3
Derek <nightwing917@hotmail.com>
2024-10-05 19:57:08 -0600

    html error hunting

--- script.js
+++ script.js
@@ -14,31 +14,7 @@ function displayUser(user) {
     document.body.style.backgroundColor = 'royalblue';
   }
 
-  userDisplay.innerHTML = '
-    <div class="flex justify-between">
-          <div class="flex">
-            <img
-              class="w-48 h-48 rounded-full mr-8"
-              src="https://randomuser.me/api/portraits/women/45.jpg"
-            />
-            <div class="space-y-3">
-              <p class="text-xl">
-                <span class="font-bold">Name: </span>${user.name.first} ${user.name.last}
-              </p>
-              <p class="text-xl">
-                <span class="font-bold">Email: </span>${user.email}
-              </p>
-              <p class="text-xl">
-                <span class="font-bold">Phone: </span> (555) 555-5555
-              </p>
-              <p class="text-xl">
-                <span class="font-bold">Location: </span> Boston MA
-              </p>
-              <p class="text-xl"><span class="font-bold">Age: </span> 30</p>
-            </div>
-          </div>
-        </div>
-  ';
+  userDisplay.innerHTML = '';
 }
 
 
